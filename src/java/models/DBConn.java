9b5f3f88f4da05c2f5444fa07854b016a6477c3f
Bilal Bayasut <bilalbayasut@gmail.com>
2016-02-01 23:01:07 +0700

    professional degree ui done

--- src/java/models/DBConn.java
+++ src/java/models/DBConn.java
@@ -14,7 +14,7 @@ import java.util.logging.Logger;
  * @author End User
  */
 public class DBConn {
-private static String host = "175.144.31.80"; 
+private static String host = "175.145.207.13"; 
 private static int netTime = 2000;
     private static String user = "root";
     private static String pass = "qwerty";
